8575bb3cdb93efabc42978125ec90c0a4cea3624
Balajin19 <bn02149@gmail.com>
2023-10-19 14:59:18 +0530

    cors

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ app.use( express.json() );
 app.use(cors());
 app.options( "*", cors() );
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://example.com");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 app.options("/", (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "https://example.com");
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.sendStatus(204);
